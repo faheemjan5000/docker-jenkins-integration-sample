207e483fa4bc2078ddd429032dddffd0382a99d7
faheemjan5000 <faheemjan500@gmail.com>
2023-05-17 10:37:46 +0200

    first commit

---
+++ src/main/java/faheem/microservices/dockerjenkinsintegrationsample/DockerJenkinsIntegrationSampleApplication.java
@@ -0,0 +1,13 @@
+package faheem.microservices.dockerjenkinsintegrationsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegrationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntegrationSampleApplication.class, args);
+	}
+
+}
