040a11ce1dd9797c39fed991672c31f3462da1f5
faheemjan5000 <faheemjan500@gmail.com>
2023-05-17 10:39:58 +0200

    adding pom file

---
+++ src/test/java/faheem/microservices/dockerjenkinsintegrationsample/DockerJenkinsIntegrationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package faheem.microservices.dockerjenkinsintegrationsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
